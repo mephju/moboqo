5308685b59adb4ba5307b35ffd92bfcfdde7a2cc
Kolja <mephju@gmail.com>
2013-02-16 20:58:59 +0100

    change html according to mraid status

--- interstitial.js
+++ interstitial.js
@@ -2,8 +2,8 @@ console.log('starting interstitial.js')
 
 if(mraid) {
 	console.log('mraid is available')
-	document.getElementById('mraid').text = 'mraid is available'
+	document.getElementById('mraid').innerHTML = 'mraid is available'
 } else {
 	console.log('mraid is not available')
-	document.getElementById('mraid').text 'mraid is not available'
+	document.getElementById('mraid').innerHTML = 'mraid is not available'
 }
