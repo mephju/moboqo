c566a144aaf9a74580fa18e64dd8bd1ec65aeaa7
Kolja <mephju@gmail.com>
2013-02-16 20:21:38 +0100

    added first mraid check

---
+++ interstitial.js
@@ -0,0 +1,9 @@
+console.log('starting interstitial.js')
+
+if(mraid) {
+	console.log('mraid is available')
+	document.getElementById('mraid').text = 'mraid is available'
+} else {
+	console.log('mraid is not available')
+	document.getElementById('mraid').text 'mraid is not available'
+}
